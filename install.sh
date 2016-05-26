c73f6f5cc8f7f5b871e55bda97b3b6d3b1ebd052
Mike Goodwin <xenithorb@users.noreply.github.com>
2016-05-26 06:34:03 +0000

    Added self-installer for my own use case

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Only intended to be use by original author
+git pull
+sudo cp -f poll_le_certs.sh /usr/local/bin/
+sudo cp -f poll_le_certs.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now poll_le_certs.service
+sudo systemctl start poll_le_certs.service
