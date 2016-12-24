0e83166ebca7faf378c243f10736da848934cf64
Michael Goodwin <xenithorb@users.noreply.github.com>
2016-12-24 17:25:19 -0500

    port_open_cron: Make script self-referential so that it can be used with
    certbot and other scripts. Now accepts "renew" to run the let's encrypt
    renewal process, which in turn runs the pre_hook, renew_hook, and
    post_hook hooks when appropriate. If not 'renew', then it simply runs
    and evaluates the entire arguments list which I am using to run internal
    functions.

--- port_open_cron/iptables_letsencrypt_cron.sh
+++ port_open_cron/iptables_letsencrypt_cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Cron to open port 80/443 for ACME protocol with 
+# Cron to open port 80/443 for ACME protocol with
 # Let's encrypt and renew system certificates.
-# 
+#
 # Author: Michael Goodwin
 # Date: 2016-05-22
 # OS: CentOS 7
@@ -14,45 +14,64 @@ CHAIN="INPUT"
 # rule number #2, check with `iptables -nvL --line-numbers`
 INSERT_NUM=2
 LE_DIR="/etc/letsencrypt/live"
+#FORCE_RENEW="--force-renew"
+FORCE_RENEW=''
+SELF="$PWD/$0"
 
 RULES=(
         "-p tcp -m conntrack --ctstate NEW -m tcp --dport 80 -j ACCEPT"
-        "-p tcp -m conntrack --ctstate NEW -m tcp --dport 443 -j ACCEPT" 
+        "-p tcp -m conntrack --ctstate NEW -m tcp --dport 443 -j ACCEPT"
 )
 
 iptables_control() {
         local cmd
         local chain="$CHAIN"
         case "$@" in
-                add*) 
+                add*)
                         cmd="-I"
                         chain="${chain} ${INSERT_NUM}"
                 ;;
-                del*) cmd="-D" ;;
+                del*)
+                        cmd="-D"
+                ;;
         esac
-        for i in "${!RULES[@]}"; do 
-                iptables $cmd $chain ${RULES[i]} 
-        done 
-} 
+        for i in "${!RULES[@]}"; do
+                iptables $cmd $chain ${RULES[i]}
+        done
+}
 
 make_all_cert() {
         local domain="$1"
         local le_dom_loc="${LE_DIR}/${domain}"
         cat "${le_dom_loc}/"{fullchain,privkey}.pem > "${le_dom_loc}/all.pem"
-} 
+}
 
-iptables_control add 
+pre_hook() {
+        iptables_control add
+}
 
-letsencrypt --agree-tos renew 
+renew_hook() {
+        while read -rd '' domain; do
+                domain="${domain##*/}"
+                make_all_cert "$domain"
+        done < <(
+                find "$LE_DIR" -mindepth 1 -maxdepth 1 -type d -print0
+        )
 
-iptables_control del 
+        # Do generic reloading of services and/or
+        # Call in another script here
 
-find "$LE_DIR" -mindepth 1 -maxdepth 1 -type d -print0| while read -rd '' domain; do
-        domain="${domain##*/}" 
-        make_all_cert "$domain"
-done 
+}
 
-# Do generic reloading of services and/or 
-# Source in another script here 
+post_hook() {
+        iptables_control del
+}
 
-#
+if [[ $1 == '' ]]; then
+        letsencrypt --agree-tos $FORCE_RENEW renew \
+                --post-hook "$SELF post_hook" \
+                --pre-hook "$SELF pre_hook" \
+                --renew-hook "$SELF renew_hook"
+else
+        eval "$@"
+fi
